9160fa423060777f4ef99a99f77b96d1ebcf1fdc
msmps <7691252+msmps@users.noreply.github.com>
2024-09-11 18:17:56 +0100

    chore: add preferred config

--- sst.config.ts
+++ sst.config.ts
@@ -6,6 +6,15 @@ export default $config({
       name: "sst-puppeteer-pdf-generator",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
+      providers: {
+        aws: {
+          region: "eu-west-2",
+          profile:
+            input.stage === "production"
+              ? "pdf-generator-prod"
+              : "pdf-generator-dev",
+        },
+      },
     };
   },
   async run() {
